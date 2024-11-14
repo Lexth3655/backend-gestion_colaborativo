dec008296a6560b2b4a4f5f3f8c1a16c801bd857
Nery02 <140977744+Nery02@users.noreply.github.com>
2024-11-13 21:10:44 -0600

    prueba

---
+++ UMicro.Domain/Modelo/UsuarioProyecto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMicro.Domain.Modelo
+{
+    internal class UsuarioProyecto
+    {
+    }
+}
